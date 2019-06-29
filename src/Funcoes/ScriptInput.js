03cb75a2bdc1ed54219b03e35dc0b374aeb931b4
João <jguilhermepasco@gmail.com>
2019-06-29 11:12:17 -0300

    Restrições de input corrigidas

--- src/Funcoes/ScriptInput.js
+++ src/Funcoes/ScriptInput.js
@@ -17,10 +17,13 @@
     //permite somente letras no input 
     function letras(){ 
     tecla = event.keyCode; 
-    if (tecla >= 33 && tecla <= 64 || tecla >= 91 && tecla <= 93 || tecla >= 123 && tecla <= 159 || tecla >= 162 && tecla <= 191 ){ 
-        return false; 
-    }else{ 
+    if ((tecla==32) ||
+        (tecla > 64 && tecla < 91) || 
+        (tecla > 96 && tecla < 123) ||
+        (tecla > 191 && tecla <= 255)){ 
         return true; 
+    }else{ 
+        return false; 
          } 
     }
     //permite somente numeros no input 
@@ -35,19 +38,26 @@
     //permite somente numeros e letras, sem caracteres especiais e espaços --> para a senha e login
     function senhaLogin(){
         tecla = event.keyCode;
-        if(tecla>=48 && tecla<=57 || tecla>=94 && tecla <=122){
+        if((tecla > 64 && tecla < 91) || 
+            (tecla > 96 && tecla < 123) ||
+            (tecla >= 48 && tecla <= 57)){
             return true;
         }else{
             return false;
         }   
     }
     //permite somente numeros e letras, sem caracteres especiais --> para o endereço
-    function endereco(){
+    function local(){
         tecla = event.keyCode;
-        if(tecla>=8 && tecla<=27 || tecla>=48 && tecla<=57 || tecla>=94 && tecla <=122){
+        if((tecla==32) ||
+            (tecla > 64 && tecla < 91) || 
+            (tecla > 96 && tecla < 123) ||
+            (tecla > 191 && tecla <= 255)||
+            (tecla >= 48 && tecla <= 57)){
             return true;
         }else{
             return false;
         }   
     }
+
 
