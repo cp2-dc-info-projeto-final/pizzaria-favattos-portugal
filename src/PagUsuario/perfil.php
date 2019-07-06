65f492b86e32e566474c76a2197fe3f06c98e4c2
João <jguilhermepasco@gmail.com>
2019-07-06 16:10:31 -0300

    Atualizando o html e css da pagina de perfil

--- src/PagUsuario/perfil.php
+++ src/PagUsuario/perfil.php
@@ -1,16 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Perfil</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../Estilo/Perfil.css">
     <link rel ="stylesheet" href ="../Estilo/bootstrap-4.1.3-dist/css/bootstrap.min.css">
     <script src="../Estilo/jquery.min.js"></script>
     <script src="../Estilo/popper.min.js"></script>
     <script src ="../Estilo/bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
 </head>
 <body>
+  <?php
+  require_once("../Funcoes/CriaConexao.php");
+  
+  $con = CriaConexao();
+  
 
+  ?>
   <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
     <!-- Logo -->
     <a class="navbar-brand" href="#">
@@ -20,7 +28,6 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="collapsibleNavbar">
-
     <!-- Links -->
     <ul class="navbar-nav">
       <li class="nav-item">
@@ -32,7 +39,6 @@
       <li class="nav-item">
             <a class="nav-link" href="#">Fotos</a>
       </li>
-    
       <!-- Dropdown !-->
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
@@ -46,35 +52,37 @@
       </li>
     </ul>
     </div>
-  </nav>
+  </nav>  
 
-<div class="container" style="margin-top:100px">
 
-  <div class="detalhes">
+  <div class="container" style="margin-top: 180px">
+  <div class="shadow p-3 mb-5 bg-white rounded">
+
+    <h1>Seus dados</h1>
     <h2>Nome: João Souza</h2>
     <h2>Idade: 16</h2>
     <h2>Email: jguilhermepasco@gmail.com</h2>
-  </div>
-  <hr>
-  <div class="infos">
+ 
+    <hr>
     <h1>Outras Informações</h1>
     <h2>Endereço: Exemplo de Endereço bem aqui!</h2>
     <h2>Telefone: 4002-8922</h2>
     <h2>Login: exemplologin</h2>
     <h2>Sexo: Masculino</h2>
-  </div>
+  
   <hr>
-  <div class="botaum">
-    <a href="Editar.php">Editar Perfil</a>
-    <a href="Sair.php">Sair</a>
+  <center>
+    <a class="btn btn-outline-success" href="Editar.php">Editar Perfil</a>
+    <a class="btn btn-outline-danger" href="Sair.php">Sair</a>
+  </center>
   </div>
-
-</div>
-
-<div class="jumbotron text-center" style="margin-bottom:0">
-    <p>Footer</p>
-</div>
-
+  </div>
+ 
+  <nav class="navbar bg-dark navbar-dark fixed-bottom">
+    <div class="container">
+      <span class="text-muted">Até que enfim foi</span>
+    </nav>
+    
 
 </body>
 </html>
